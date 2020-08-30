b56f5ca139d0d5effefe2371d28cf0bca72d210b
SALajes <sofialajes98@gmail.com>
2020-08-30 21:13:43 +0100

    Footer ready

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,12 +8,12 @@ const Footer = () => (
             backgroundColor: 'var(--dark-grey)',
             color: 'var(--white)',
             font: '0.8rem',
-            textAlign: "center",
-            padding: "0.5rem",
-            position: "fixed",
-            left: "0",
-            bottom: "0",
-            width: "100%",
+            textAlign: 'center',
+            padding: '0.5rem',
+            position: 'absolute',
+            bottom: 0,
+            left:0,
+            right:0
         }}
     >
         A TUNAFE @ Desde 1992
