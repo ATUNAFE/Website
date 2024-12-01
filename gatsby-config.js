b6fd2d8775cb2acb864ecd63136d7b669c8132b4
catarina03 <f.catarina03@gmail.com>
2024-12-01 20:59:55 +0000

    Update configurations

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,8 @@ module.exports = {
       },
     },
      // other plugins
-     `gatsby-plugin-decap-cms`
+     `gatsby-adapter-netlify`,
+     `gatsby-plugin-decap-cms`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
