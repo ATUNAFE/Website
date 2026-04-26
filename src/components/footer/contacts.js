8b7b8da9f47ee324fcf6f809a0263736bc05e395
Inês <ines.soares.22102003@gmail.com>
2026-04-26 20:19:00 +0100

    Fixed bug

--- src/components/footer/contacts.js
+++ src/components/footer/contacts.js
@@ -6,58 +6,66 @@ import { IMAGE_FILENAMES } from "../../utils/constants";
 
 const Contacts = ({ magister, rp, email, address }) => (
     <Container style={{ fontSize: "13px" }}>
-        <h5 className="text-start" style={{ fontWeight: "bold" }}>Contactos</h5>
+        <h5 className="text-start" style={{ fontWeight: "bold", marginBottom: "15px" }}>Contactos</h5>
 
         {/* RP */}
-        <Row className="align-items-center mb-2" >
-            <Col xs="4" md="2" className="text-center">
-                <CustomImage
-                    src={IMAGE_FILENAMES.pages.white.telefone}
-                    alt="Phone"
-                />
+        <Row className="align-items-center mb-2 flex-nowrap">
+            <Col xs={2} md={2} className="text-center">
+                <div style={{ width: "20px" }}>
+                    <CustomImage
+                        src={IMAGE_FILENAMES.pages.white.telefone}
+                        alt="Phone"
+                    />
+                </div>
             </Col>
-            <Col xs="14" md="10" className="text-start">
+            <Col xs={10} md={10} className="text-start">
                 <p className="mb-0">{`${rp.name} (Relações Públicas)`}</p>
                 <p className="mb-0">{rp.phone}</p>
             </Col>
         </Row>
 
         {/* Magister */}
-        <Row className="align-items-center mb-2">
-            <Col xs="4" md="2" className="text-center">
-                <CustomImage
-                    src={IMAGE_FILENAMES.pages.white.telefone}
-                    alt="Phone"
-                />
+        <Row className="align-items-center mb-2 flex-nowrap">
+            <Col xs={2} md={2} className="text-center">
+                <div style={{ width: "20px" }}>
+                    <CustomImage
+                        src={IMAGE_FILENAMES.pages.white.telefone}
+                        alt="Phone"
+                    />
+                </div>
             </Col>
-            <Col xs="14" md="10" className="text-start">
+            <Col xs={10} md={10} className="text-start">
                 <p className="mb-0">{`${magister.name} (Magister)`}</p>
                 <p className="mb-0">{magister.phone}</p>
             </Col>
         </Row>
 
         {/* E-mail */}
-        <Row className="align-items-center mb-2">
-            <Col xs="4" md="2" className="text-center">
-                <CustomImage
-                    src={IMAGE_FILENAMES.pages.white.mail}
-                    alt="Phone"
-                />
+        <Row className="align-items-center mb-2 flex-nowrap">
+            <Col xs={2} md={2} className="text-center">
+                <div style={{ width: "20px" }}>
+                    <CustomImage
+                        src={IMAGE_FILENAMES.pages.white.mail}
+                        alt="Mail"
+                    />
+                </div>
             </Col>
-            <Col xs="14" md="10" className="text-start">
+            <Col xs={10} md={10} className="text-start">
                 <p className="mb-0">{email}</p>
             </Col>
         </Row>
 
         {/* Morada */}
-        <Row className="align-items-center">
-            <Col xs="4" md="2" className="text-center">
-                <CustomImage
-                    src={IMAGE_FILENAMES.pages.white.morada}
-                    alt="Phone"
-                />
+        <Row className="align-items-center flex-nowrap">
+            <Col xs={2} md={2} className="text-center">
+                <div style={{ width: "20px" }}>
+                    <CustomImage
+                        src={IMAGE_FILENAMES.pages.white.morada}
+                        alt="Address"
+                    />
+                </div>
             </Col>
-            <Col xs="14" md="10" className="text-start">
+            <Col xs={10} md={10} className="text-start">
                 {address.split("\n").map((line, index) => (
                     <p key={index} className="mb-0">
                         {line}
