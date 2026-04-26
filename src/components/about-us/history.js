7a09c7461ef8014c32f6e647b5422e0544338b18
Inês <ines.soares.22102003@gmail.com>
2026-04-26 18:17:12 +0100

    Add quote inHistorial section

--- src/components/about-us/history.js
+++ src/components/about-us/history.js
@@ -34,9 +34,31 @@ const History = ({ id }) => {
             id={id} 
             style={{
                 position: "relative",
-                backgroundColor: "var(--light-neutral)"
+                backgroundColor: "var(--light-neutral)",
+                overflow: "hidden"
             }}
         >
+            {/* Injecting CSS specifically for the Markdown output */}
+            <style>{`
+                .history-content blockquote {
+                    border-left: none;
+                    margin-top: 2.5rem;
+                    padding: 0;
+                    font-style: italic;
+                    color: inherit;
+                }
+                .history-content blockquote p {
+                    margin-bottom: 0.5rem;
+                    text-align: justify;
+                }
+                /* Targets the last paragraph in a blockquote (the author) */
+                .history-content blockquote p:last-child {
+                    text-align: right;
+                    font-style: normal;
+                    margin-top: 1rem;
+                }
+            `}</style>
+
             <CustomImage
                 src={content.frontmatter.watermark}
                 style={{
@@ -51,30 +73,29 @@ const History = ({ id }) => {
                     pointerEvents: "none",
                 }}
             />
-            <Container className="py-4" style={{ position: "relative", zIndex: 2 }}>
-                <Row>
-                    <h3>{content.frontmatter.title.text}</h3>
+            <Container className="py-5" style={{ position: "relative", zIndex: 2 }}>
+                <Row className="mb-4">
+                    <Col>
+                        <h2 style={{ fontWeight: "bold" }}>{content.frontmatter.title.text}</h2>
+                    </Col>
                 </Row>
-                <Row>
-                    <Col xs={6} md={4} className="d-flex flex-column justify-content-center align-items-center">
+                <Row className="align-items-center">
+                    <Col xs={12} md={4} className="d-flex justify-content-center mb-4 mb-md-0">
                         <CustomImage
                             src={content.frontmatter.image}
+                            style={{ width: "100%", maxWidth: "300px" }}
                         />
                     </Col>
-                    <Col xs={12} md={8} className="d-flex flex-column justify-content-center">
-                        <div style={{ textAlign: "justify" }}>
+                    <Col xs={12} md={8}>
+                        <div className="history-content">
                             <div dangerouslySetInnerHTML={{ __html: content.html }} />
                         </div>
-                        {/* <div className="text-center">
-                        <p><i>{citation.text}</i></p>
-                    </div>
-                    <div className="text-end">
-                        <p><i>{citation.author}</i></p>
-                    </div> */}
                     </Col>
                 </Row>
-                <Row className="mt-4">
-                    <MultiImageCarousel images={content.frontmatter.carousel} />
+                <Row className="mt-5">
+                    <Col>
+                        <MultiImageCarousel images={content.frontmatter.carousel} />
+                    </Col>
                 </Row>
             </Container>
         </div>
