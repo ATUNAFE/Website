becf2af539f85af21fdf6ca55f8ba9fa3098dc6d
Inês <ines.soares.22102003@gmail.com>
2026-05-05 13:17:40 +0100

    I think is okay now

--- src/components/footer/follow-us.js
+++ src/components/footer/follow-us.js
@@ -1,44 +1,55 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-
+import { Container } from "react-bootstrap";
 import CustomImage from "../images/image";
 import { IMAGE_FILENAMES } from "../../utils/constants";
 
-const FollowUs = ({ socialMedia }) => (
-    <Container>
-        <h5 className="text-start mb-4" style={{ fontWeight: "bold" }}>Segue-nos</h5>
-        <Row className="justify-content-start g-3">
-            <Col xs={4} md={2} className="text-center">
-                <a href={socialMedia.instagram} target="_blank" rel="noopener noreferrer">
-                    <CustomImage src={IMAGE_FILENAMES.pages.white.instagram} alt="Instagram" />
-                </a>
-            </Col>
+const FollowUs = ({ socialMedia }) => {
+    const iconWrapperStyle = {
+        width: "60px",
+        height: "60px", 
+        marginRight: "15px",
+        marginBottom: "15px",
+        display: "inline-block"
+    };
 
-            <Col xs={4} md={2} className="text-center">
-                <a href={socialMedia.facebook} target="_blank" rel="noopener noreferrer">
-                    <CustomImage src={IMAGE_FILENAMES.pages.white.facebook} alt="Facebook" />
-                </a>
-            </Col>
+    return (
+        <Container>
+            <h5 className="text-start mb-4" style={{ fontWeight: "bold" }}>Segue-nos</h5>
+            <div className="d-flex flex-wrap justify-content-start">
+                
+                <div style={iconWrapperStyle}>
+                    <a href={socialMedia.instagram} target="_blank" rel="noopener noreferrer">
+                        <CustomImage src={IMAGE_FILENAMES.pages.white.instagram} alt="Instagram" />
+                    </a>
+                </div>
 
-            <Col xs={4} md={2} className="text-center">
-                <a href={socialMedia.youtube} target="_blank" rel="noopener noreferrer">
-                    <CustomImage src={IMAGE_FILENAMES.pages.white.youtube} alt="Youtube" />
-                </a>
-            </Col>
+                <div style={iconWrapperStyle}>
+                    <a href={socialMedia.facebook} target="_blank" rel="noopener noreferrer">
+                        <CustomImage src={IMAGE_FILENAMES.pages.white.facebook} alt="Facebook" />
+                    </a>
+                </div>
 
-            <Col xs={4} md={2} className="text-center">
-                <a href={socialMedia.linkedin} target="_blank" rel="noopener noreferrer">
-                    <CustomImage src={IMAGE_FILENAMES.pages.white.linkedin} alt="Linkedin" />
-                </a>
-            </Col>
+                <div style={iconWrapperStyle}>
+                    <a href={socialMedia.youtube} target="_blank" rel="noopener noreferrer">
+                        <CustomImage src={IMAGE_FILENAMES.pages.white.youtube} alt="Youtube" />
+                    </a>
+                </div>
 
-            <Col xs={4} md={2} className="text-center">
-                <a href={socialMedia.spotify} target="_blank" rel="noopener noreferrer">
-                    <CustomImage src={IMAGE_FILENAMES.pages.white.spotify} alt="Spotify" />
-                </a>
-            </Col>
-        </Row>
-    </Container>
-);
+                <div style={iconWrapperStyle}>
+                    <a href={socialMedia.linkedin} target="_blank" rel="noopener noreferrer">
+                        <CustomImage src={IMAGE_FILENAMES.pages.white.linkedin} alt="Linkedin" />
+                    </a>
+                </div>
+
+                <div style={iconWrapperStyle}>
+                    <a href={socialMedia.spotify} target="_blank" rel="noopener noreferrer">
+                        <CustomImage src={IMAGE_FILENAMES.pages.white.spotify} alt="Spotify" />
+                    </a>
+                </div>
+
+            </div>
+        </Container>
+    );
+};
 
 export default FollowUs;
