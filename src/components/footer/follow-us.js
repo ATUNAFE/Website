fd54b022f1db19cef185e0bdf7d5417dd3ec0972
Inês <ines.soares.22102003@gmail.com>
2026-05-05 13:27:04 +0100

    Adjusted alignment

--- src/components/footer/follow-us.js
+++ src/components/footer/follow-us.js
@@ -1,53 +1,45 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import CustomImage from "../images/image";
 import { IMAGE_FILENAMES } from "../../utils/constants";
 
 const FollowUs = ({ socialMedia }) => {
-    const iconWrapperStyle = {
-        width: "60px",
-        height: "60px", 
-        marginRight: "15px",
-        marginBottom: "15px",
-        display: "inline-block"
+    const iconStyle = {
+        width: "45px",
+        height: "auto",
+        display: "block"
     };
 
     return (
         <Container>
             <h5 className="text-start mb-4" style={{ fontWeight: "bold" }}>Segue-nos</h5>
-            <div className="d-flex flex-wrap justify-content-start">
-                
-                <div style={iconWrapperStyle}>
-                    <a href={socialMedia.instagram} target="_blank" rel="noopener noreferrer">
-                        <CustomImage src={IMAGE_FILENAMES.pages.white.instagram} alt="Instagram" />
-                    </a>
-                </div>
+            
+            <Row>
+                <Col xs={2} md={2}></Col>
+                <Col xs={10} md={10}>
+                    <div className="d-flex flex-wrap gap-3">
+                        <a href={socialMedia.instagram} target="_blank" rel="noopener noreferrer" style={iconStyle}>
+                            <CustomImage src={IMAGE_FILENAMES.pages.white.instagram} alt="Instagram" />
+                        </a>
 
-                <div style={iconWrapperStyle}>
-                    <a href={socialMedia.facebook} target="_blank" rel="noopener noreferrer">
-                        <CustomImage src={IMAGE_FILENAMES.pages.white.facebook} alt="Facebook" />
-                    </a>
-                </div>
+                        <a href={socialMedia.facebook} target="_blank" rel="noopener noreferrer" style={iconStyle}>
+                            <CustomImage src={IMAGE_FILENAMES.pages.white.facebook} alt="Facebook" />
+                        </a>
 
-                <div style={iconWrapperStyle}>
-                    <a href={socialMedia.youtube} target="_blank" rel="noopener noreferrer">
-                        <CustomImage src={IMAGE_FILENAMES.pages.white.youtube} alt="Youtube" />
-                    </a>
-                </div>
+                        <a href={socialMedia.youtube} target="_blank" rel="noopener noreferrer" style={iconStyle}>
+                            <CustomImage src={IMAGE_FILENAMES.pages.white.youtube} alt="Youtube" />
+                        </a>
 
-                <div style={iconWrapperStyle}>
-                    <a href={socialMedia.linkedin} target="_blank" rel="noopener noreferrer">
-                        <CustomImage src={IMAGE_FILENAMES.pages.white.linkedin} alt="Linkedin" />
-                    </a>
-                </div>
+                        <a href={socialMedia.linkedin} target="_blank" rel="noopener noreferrer" style={iconStyle}>
+                            <CustomImage src={IMAGE_FILENAMES.pages.white.linkedin} alt="Linkedin" />
+                        </a>
 
-                <div style={iconWrapperStyle}>
-                    <a href={socialMedia.spotify} target="_blank" rel="noopener noreferrer">
-                        <CustomImage src={IMAGE_FILENAMES.pages.white.spotify} alt="Spotify" />
-                    </a>
-                </div>
-
-            </div>
+                        <a href={socialMedia.spotify} target="_blank" rel="noopener noreferrer" style={iconStyle}>
+                            <CustomImage src={IMAGE_FILENAMES.pages.white.spotify} alt="Spotify" />
+                        </a>
+                    </div>
+                </Col>
+            </Row>
         </Container>
     );
 };
